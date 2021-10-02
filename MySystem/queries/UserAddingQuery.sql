e2dec8a65eba3032f5aabc83e435cb22333596db
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-10-02 11:50:37 +0500

    clean up

--- MySystem/queries/UserAddingQuery.sql
+++ MySystem/queries/UserAddingQuery.sql
@@ -1,8 +1,4 @@
 use [Sarvar Bank]
---drop table Users
---insert into Users(PhoneNumber, UserType, FirstName,MiddleName,LastName,BirthDate,Gender, MaritalStatus, CitizenShip) 
---values	('935056800', 'A','Сарвар','Мамадамонович','Абдуллоев','2005-06-22','M','U',	'T');
---delete from Users where ID = 1
---я редактировал этот файл в SSMS. В vs code кирилица не хочет нормально показыватся 
---select * from Users where PhoneNumber = '935056800'
-select * from Users
+
+--select * from Users
+select * from Credits
